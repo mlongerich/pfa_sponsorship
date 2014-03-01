86e223ffa2dbc1dd984635669917e2af4919f77c
Michael Marcel Longerich <mlongerich@gmail.com>
2014-03-01 15:53:11 +0700

    Makes columns for children sortable

--- app/controllers/children_controller.rb
+++ app/controllers/children_controller.rb
@@ -1,7 +1,8 @@
 class ChildrenController < ApplicationController
   before_action :fetch_child, only: [:show, :edit, :update, :destroy, :toggle_active, :toggle_living, :toggle_schooling] 
+  helper_method :sort_column, :sort_direction 
   def index
-    @children = Child.all
+    @children = Child.order(sort_column + " " + sort_direction)
 
   end
 
@@ -42,8 +43,15 @@ class ChildrenController < ApplicationController
 
   private
 
-
   def fetch_child
     @child = Child.find(params[:id])
   end
+
+  def sort_column
+    Child.column_names.include?(params[:sort]) ? params[:sort] : "id"
+  end
+
+  def sort_direction
+    %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
+  end 
 end
