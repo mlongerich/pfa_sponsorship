81f0acc01f6edc37b2a51e9b20bbface917baf1e
Michael Marcel Longerich <mlongerich@gmail.com>
2014-04-28 19:07:58 +0700

    Stuff I forgot to commit

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,8 @@ module ApplicationHelper
     direction = column == sort_column && sort_direction == "asc" ? "desc" : "asc"
     link_to title, sort: column, direction: direction, table: table  
   end
+  def st_field(stid)
+		st = SponsorshipType.find(stid)
+		st.kind + " " + st.amount.to_s
+	end
 end
